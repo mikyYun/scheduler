88ae7f2ce588a44310cc2cf2fa36d5abee57e274
mikyYun <mkyun2714@gmail.com>
2022-05-17 01:01:39 -0700

    test passed application.js and button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,29 @@
 import React from "react";
+import classNames from 'classnames';
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   console.log('PPP', props.onClick)
+   // let buttonClass = 'button';
+   // let className = {
+   //    buttonClass,
+   //    'button--confirm': props.confirm,
+   //    'button--danger': props.danger
+   // }
+   let buttonClass = classNames('button', {
+      'button--confirm': props.confirm,
+      'button--danger': props.danger
+   })
+
+
+
+
+   // if props has trythy for onClick || disabled...
+   return (
+   <button className={buttonClass} 
+   onClick={props.onClick} 
+   disabled={props.disabled}>
+      {props.children}
+   </button>);
 }
