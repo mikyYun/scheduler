61513c9803998f206bb3b03337e52f60d61c6bca
mikyYun <mkyun2714@gmail.com>
2022-05-20 23:24:11 -0700

    add getInterviewersForDay helper

--- src/helpers/selectors.jsx
+++ src/helpers/selectors.jsx
@@ -14,28 +14,23 @@ export function getAppointmentsForDay(state, day) {
 
 export function getInterview(state, interview) {
   if (interview === null) return null;
-  // const result = interview
-  // console.log(result)
-  // result.interviewer = state.interviewers[interview.interviewer]
-  // return result;
+
   return {
     student: interview.student,
-    interviewer: state.interviewers[interview.interviewer]
+    interviewer: state.interviewers[interview.interviewer],
+  };
+
+}
+
+export function getInterviewersForDay (state, day) {
+  const result = []
+  const matchingDay = state.days.filter((each) => each.name === day)
+  console.log(matchingDay[0])
+  if (matchingDay.length > 0) {
+    const interviewersID = matchingDay[0].interviewers
+    interviewersID.forEach((each) => {
+      result.push(state.interviewers[each])
+    })
   }
+  return result
 };
-
-// state = {
-//   day: "Monday",
-// days: [],
-// appointments: {},
-// interviewers: {}
-// }
-
-
-// expect.objectContaining({
-//   student: expect.any(String),
-//   interviewer: expect.objectContaining({
-//     id: expect.any(Number),
-//     name: expect.any(String),
-//     avatar: expect.any(String)
-//   })
