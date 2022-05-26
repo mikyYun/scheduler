203bc204036cf4ba9918ac39304ba6236e5a81f8
mikyYun <mkyun2714@gmail.com>
2022-05-25 23:31:48 -0700

    async test to container full loaded

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,33 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
 
-afterEach(cleanup);
+describe("Application", () => {
+  afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
-});
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText("Monday"));
+  
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    // expect(getByText("Leopold Silve")).toBeInTheDocument();
+  });
+  
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+    console.log("jest container", container) // obj of HTTPDivElement
+    console.log('prettyDOM', prettyDOM(container)) // DOM tree of initial state of the application // data hasn't loaded yet
+    
+    // await before prettyDOM call
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+
+    console.log("after await", prettyDOM(container))
+
+
+
+  });
+})
